3d5abf2be6ee3f3775329d31f52469756fadb84e
Manuel Riecke <api.beast@gmail.com>
2021-03-09 17:16:38 +0100

    Trigger assert on failed import.

--- slick/slick.hpp
+++ slick/slick.hpp
@@ -12,7 +12,7 @@ inline std::string import_file(const char* file_name)
 	std::ifstream file(file_name, std::ios::binary);
 	std::string buffer{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
 	buffer.push_back(0x0); // Null terminator
-	assert(buffer.size() > 0);
+	assert(buffer.size() > 1);
 	return buffer;
 }
 
